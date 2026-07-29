8897344b79f036f7b13a335a4c6d57ef8abc796a
sanda <ioanasandu815@gmail.com>
2026-07-29 14:22:27 +0300

    delethe de email form

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,33 +41,7 @@ const blurHeader = () =>{
 window.addEventListener('scroll', blurHeader)
 
 /*=============== EMAIL JS ===============*/
-const contactForm = document.getElementById('contact-form'),
-      contactMessage = document.getElementById('contact-message')
 
-const sendEmail = (e) =>{
-   e.preventDefault()
-
-   // serviceID - templateID - #form - publicKey
-   emailjs.sendForm('','','#contact-form','')
-      .then(() =>{
-         // Show sent message
-         contactMessage.textContent = 'Message sent successfully ✅'
-
-         // Remove message after five seconds
-         setTimeout(() =>{
-            contactMessage.textContent = ''
-         }, 5000)
-
-         // Clear input fields
-         contactForm.reset()
-
-      }, () =>{
-         // Show error message
-         contactMessage.textContent = 'Message not sent (service error) ❌'
-      })
-}
-
-contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 const scrollUp = () =>{
