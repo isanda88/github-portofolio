81c266d919c5bad9163d0ffa46f01ca84123f983
sanda <ioanasandu815@gmail.com>
2026-08-03 15:11:52 +0300

    delete the contact js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,32 +1,32 @@
-const contactForm = document.getElementById("contact-form");
-const contactMessage = document.getElementById("contact-message");
+// const contactForm = document.getElementById("contact-form");
+// const contactMessage = document.getElementById("contact-message");
 
-emailjs.init({
-    publicKey: "kVek1Xe6s7uQlLIJs",
-});
+// emailjs.init({
+//     publicKey: "kVek1Xe6s7uQlLIJs",
+// });
 
-contactForm.addEventListener("submit", function (e) {
-    e.preventDefault();
+// contactForm.addEventListener("submit", function (e) {
+//     e.preventDefault();
 
-    emailjs
-        .sendForm(
-            "service_chxvu88",
-            "template_y3aa7aw",
-            "#contact-form"
-        )
-        .then(() => {
-            contactMessage.textContent = "Message sent successfully!";
-            contactMessage.style.color = "green";
-            contactForm.reset();
+//     emailjs
+//         .sendForm(
+//             "service_chxvu88",
+//             "template_y3aa7aw",
+//             "#contact-form"
+//         )
+//         .then(() => {
+//             contactMessage.textContent = "Message sent successfully!";
+//             contactMessage.style.color = "green";
+//             contactForm.reset();
 
-            setTimeout(() => {
-                contactMessage.textContent = "";
-            }, 5000);
-        })
-        .catch((error) => {
-    console.error("EmailJS Error:", error.text || error);
+//             setTimeout(() => {
+//                 contactMessage.textContent = "";
+//             }, 5000);
+//         })
+//         .catch((error) => {
+//     console.error("EmailJS Error:", error.text || error);
 
-    contactMessage.textContent = "Error: " + (error.text || "Unknown error");
-    contactMessage.style.color = "red";
-});
-});
+//     contactMessage.textContent = "Error: " + (error.text || "Unknown error");
+//     contactMessage.style.color = "red";
+// });
+// });
